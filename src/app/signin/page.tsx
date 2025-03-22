2303f1ecb9e51b358cb346986663527d93d3e139
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-03-22 15:47:41 +0500

    remove one picture and fix signin route

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,15 +1,9 @@
-"use client";
-
 import { SignIn } from "@clerk/nextjs";
-import { useSearchParams } from "next/navigation";
 
 const SignInPage = () => {
-  const searchParams = useSearchParams();
-  const redirectUrl = searchParams.get("redirect") || "/";
-
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignIn redirectUrl={redirectUrl} />
+      <SignIn redirectUrl="/" />
     </div>
   );
 };
